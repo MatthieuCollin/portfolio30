f34e395cfc4223a07ada5912c55afded0e49284a
Demo <matthcollin6@gmail.com>
2024-03-13 15:07:43 +0100

    👌 IMPROVE: clean assets

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,33 +1,7 @@
 import profil from './profil.png'
-import nico from './IMG_1054.png'
-import melvin from './1681284169738.jpeg'
-import erwan from './1675104812223.jpeg'
-import sitecd from './sitecd.png'
-import logoTips from './tips-logo.png'
-import siteKaraoke from './siteKaraoke.png'
-import ancienSite from './ancienPortfolio.png'
-import frontMentor from './frontmentor.png'
-import dotnet from './dotnet@home.png'
-import portfolio from './sitePortfolio.png'
-import scholarfab from './adenLogo.jpeg'
-import laposte from './laposte.png'
-import FacCaen from './facCaen.png'
 import loader from './loader.png'
 
 export {
     profil,
-    nico,
-    melvin,
-    erwan,
-    sitecd,
-    logoTips,
-    scholarfab,
-    dotnet,
-    frontMentor,
-    ancienSite,
-    siteKaraoke,
-    portfolio,
-    laposte,
-    FacCaen,
     loader
 }
