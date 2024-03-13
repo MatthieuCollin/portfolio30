73213b5df9b33b4c852bd3a5cc0a32ee3c456196
Demo <matthcollin6@gmail.com>
2024-03-13 14:58:13 +0100

    🐛 FIX: loader-div

--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -3,7 +3,7 @@ import { loader } from '../../assets/img';
 const Loader = (props) =>{
     return (
         <div className="loader">
-            <div>
+            <div className='loader-div'>
                 <img src={loader}></img>
                 <div class="progress-bar">
                     <div class="progress" style={{width:`${props.loadingState * 100 / props.dataLength}%`}}></div>
