7925daa12b5fcbffb5412c4717b016ba380e9d66
Demo <matthcollin6@gmail.com>
2024-03-13 11:22:51 +0100

    📦 NEW: ProgressBar loader

--- src/app/App.js
+++ src/app/App.js
@@ -12,41 +12,60 @@ import Loader from '../components/loader';
 function App() {
 
   const [data, setData] = useState({profil: null,works: null, state: false,institutions: null, skills: null});
+  const [loadingState, setLoadingState] = useState(0);
 
     useEffect(()=>{
         getData();
     },[])
     
     async function getData(){
+        let currentLoadingState = 0;
         let profil = await fetchApi("profils");
+        currentLoadingState++
+        setLoadingState(currentLoadingState)
         let works = await fetchApi("works");
+        currentLoadingState++
+        setLoadingState(currentLoadingState)
         let skills = await fetchApi("skills")
+        currentLoadingState++
+        setLoadingState(currentLoadingState)
         let institutions = await fetchApi("institutions")
+        currentLoadingState++
+        setLoadingState(currentLoadingState)
 
-        setData({
-          profil: profil["hydra:member"][0],
-          works: works["hydra:member"],
-          skills: skills["hydra:member"],
-          institutions: institutions["hydra:member"],
-          state:true
-        });
+        currentLoadingState++
+        setLoadingState(currentLoadingState)
+        setTimeout(()=>{
+          setData({
+            profil: profil["hydra:member"][0],
+            works: works["hydra:member"],
+            skills: skills["hydra:member"],
+            institutions: institutions["hydra:member"],
+            state:true
+          });
+          
+        }, 1000)
+      
     };
 
     
     
   return (
     <>
-    {data.state ?  <div className="App">
-    <Sidebar key="103"/>
-    <div className="box">
-      <Homepage profil={data.profil} key="1"/>
-      <Profil profil={data.profil}/>
-      <Portfolio works={data.works} key="3"/>
-      <Experiences skills={data.skills} institutions={data.institutions} key="5" />
-      <Contact profil={data.profil} key="8"/>
-      <Footer key="11"/>
-    </div>
-  </div> :<Loader />}
+    {data.state ?  
+    <div onLoad={(e)=> e.currentTarget.classList.add("loaded")} className={`App`}>
+      <Sidebar key="103"/>
+      <div className="box">
+        <Homepage profil={data.profil} key="1"/>
+        <Profil profil={data.profil}/>
+        <Portfolio works={data.works} key="3"/>
+        <Experiences skills={data.skills} institutions={data.institutions} key="5" />
+        <Contact profil={data.profil} key="8"/>
+        <Footer key="11"/>
+      </div>
+    </div> 
+  :
+  <Loader loadingState={loadingState} dataLength={6}/>}
   </>
 
   );
--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -1,10 +1,13 @@
 import { loader } from '../../assets/img';
 import './index.css'
 
-const Loader = () =>{
+const Loader = (props) =>{
     return (
         <div className="loader">
             <img src={loader}></img>
+            <div class="progress-bar">
+                <div class="progress" style={{width:`${props.loadingState * 100 / props.dataLength}%`}}></div>
+            </div>
         </div>
     )
 }
